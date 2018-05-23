11ccb45c3d1d3b4d1697f97ae3f837d84b507e29
Razshal <fontenille.mathieu@gmail.com>
2018-05-23 14:34:29 +0200

    multiples instructions wip

--- src/display.c
+++ src/display.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <mfonteni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/21 12:36:15 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/05/22 14:50:11 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/05/22 15:21:31 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 void he_lives (t_champ *champ, int display_flag)
 {
 	if (display_flag && champ)
-		ft_printf("{BLUE}Champion %d is alive{EOC}", champ->id);
+		ft_printf("[%d]{BLUE}Champion %d is alive{EOC}", champ->lives, champ->id);
 }
--- src/instructions.c
+++ src/instructions.c
@@ -6,11 +6,11 @@
 /*   By: mfonteni <mfonteni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/21 12:36:15 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/05/22 14:50:25 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/05/23 14:17:36 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//fonction qui dechiffre l'encodage des parametres
+//TODO: fonction qui dechiffre l'encodage des parametres
 #include "../include/corewar.h"
 
 int live(t_champ *champ_list, int champ_num, int display_flag)
@@ -25,10 +25,70 @@ int live(t_champ *champ_list, int champ_num, int display_flag)
 	he_lives(champ, display_flag);
 	return (1);
 }
-
+//not sure about this one :
 int ld(t_process *process, int value, int *reg)
 {
 	if (!reg)
 		return (process->carry = 0);
+	*reg = value;
+	return (process->carry = 1);
+}
+
+int lld()
+{
+	
+}
+
+int st(t_process *process, int *reg1, int *reg2)
+{
+	if (!reg1 || !reg2)
+		return (process->carry = 0);
+	*reg2 = *reg1;
+	return (process->carry = 1);
+}
+
+int sti()
+{
+
+}
+//Elle prendra un index, et fait un saut à cette adresse si le carry est à 1.
+//TODO: What do they mean by "jump" ? Do we have to move the PC ?
+int zjmp()
+{
+	
+}
+// prend 2 index et 1 registre, additionne les 2 premiers, 
+// traite ca comme une adresse, y lit une valeur de la taille d’un 
+// registre et la met dans le 3eme.
+int ldi()
+{
+
+}
+
+int lldi()
+{
+	
+}
+
+int aff(int *reg)
+{
+	if (!reg)
+		return (0);
+	ft_putchar(*reg % 256);
+	return (1);
+}
+
+int fork()
+{
+
+}
+
+int lfork()
+{
+	
+}
+
+int st()
+{
 	
 }
