32daf8376511835e030b1b06adddcfe422458e4e
Razshal <fontenille.mathieu@gmail.com>
2018-05-28 15:41:55 +0200

    ocp wip

--- include/corewar.h
+++ include/corewar.h
@@ -6,19 +6,21 @@
 /*   By: mfonteni <mfonteni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 17:40:13 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/05/28 12:32:24 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/05/28 14:53:40 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COREWAR_H
 #define COREWAR_H
+#define REG 10
+#define DIRECT 20
+#define INDIRECT 30
 
 #include "../libft/includes/libft.h"
 #include "op.h"
 #include <fcntl.h>
 #include <sys/types.h>
 #include <unistd.h>
-
 
 typedef struct	s_champ
 {
@@ -52,14 +54,20 @@ typedef struct	s_vm
 	struct s_process	*processes;
 }				t_vm;
 
+typedef struct	s_param_instr
+{
+	int		value;
+	void	*address;
+	int		active;
+}				t_param_instr;
+
 typedef struct	s_instr
 {
-	int value;
-	int *adress;
-	int active;
+	t_process		*process;
+	int				opcode;
+	t_param_instr	params[3];
+	int				cycles_left;
 }				t_instr;
-
-
 
 //free.c
 int				free_vm(t_vm *vm);
@@ -68,10 +76,10 @@ int				free_vm(t_vm *vm);
 t_champ			*init_champs(int ac, char **av);
 
 //init.c
-t_vm		*init_vm(int argc, char **argv, int opt);
+t_vm			*init_vm(int argc, char **argv, int opt);
 
 //get_champ.c
-t_champ		*parse_champ(char *file_name , t_champ *champ);
+t_champ			*parse_champ(char *file_name , t_champ *champ);
 
 //error.c
 int				usage(void);
@@ -80,6 +88,5 @@ int				error_mall(int err);
 
 //getters.c
 t_champ			*get_champ_by_num(t_champ *list, int num);
-
 
 #endif
