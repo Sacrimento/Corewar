77371dfc47b6fb7a42c2beb8297ee5ad967c91fd
Razshal <fontenille.mathieu@gmail.com>
2018-06-01 16:44:40 +0200

    ocp debug wip

--- include/corewar.h
+++ include/corewar.h
@@ -6,7 +6,7 @@
 /*   By: mfonteni <mfonteni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 17:40:13 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/06/01 14:58:26 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/06/01 16:40:41 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,7 @@
 /********************DEBUG KINGDOM*********************/
 #define ERROR(message) ft_printf("{RED}%s{EOC}\n", message)
 #define INFO(message) ft_printf("{BLUE}%s{EOC}\n", message)
-
-
+#define INFONUM(message) ft_printf("{BLUE}%d{EOC}\n", message)
 /******************************************************/
 
 typedef struct			s_champ
@@ -104,7 +103,11 @@ int						opt(char **av, int ac, int *i, t_vm *vm);
 //instr_params.c
 t_param					*decode_param_type(unsigned char ocp);
 t_param					*get_params(t_vm *vm, t_process *process);
-int						continue_process(t_vm *vm, t_process *process);
 t_instr					instr_params(t_vm *vm, t_process *process);
+int						continue_process(t_vm *vm, t_process *process);
+
+//instructions.c
+int						live(t_instr instr);
+int						continue_process(t_vm *vm, t_process *process);
 
 #endif
--- src/TEMP_test_instr.c
+++ src/TEMP_test_instr.c
@@ -8,40 +8,20 @@ void 	inttounsignedchar(int n, unsigned char *map)
 	map[3] = n & 0xFF;
 }
 
-int		live(t_instr instr)
-{
-	t_champ *thischamp;
-
-	if (!instr.vm->champ || !instr.params)
-	{
-		ERROR("paramerror");
-		return (0);
-	}
-	if (!instr.vm || !instr.vm->champ
-	|| !(thischamp = get_champ_by_num(instr.vm->champ, 
-	instr.params[1].value)))
-		return (0);
-	thischamp->lives++;
-	ft_printf("[%d]{BLUE}Champion %s(id:%d) is alive{EOC}",
-		thischamp->lives, thischamp->name, thischamp->id);
-	return (1);
-}
-
 int main(void)
 {
 	t_vm		*vm = ft_memalloc(sizeof(t_vm));
 	t_champ		*🅱️hampion = ft_memalloc(sizeof(t_champ));
 	t_process	*process = ft_memalloc(sizeof(t_process));
-	t_instr		instr;
 
 	if (!vm || !(vm->map = (unsigned char *)ft_memalloc(MEM_SIZE)))
 		ERROR("Cannot initiate test");
 	vm->map[0] = 1;
+	vm->map[1] = 1;
 	🅱️hampion->id = 1;
 	vm->champ = 🅱️hampion;
 	ft_strcat(🅱️hampion->name, "tamere");
-	instr = instr_params(vm, process);
 	inttounsignedchar(1, &vm->map[1]);
-	if (!live(instr))
+	if (!continue_process(vm, process))
 		ERROR("FAIL");
 }
