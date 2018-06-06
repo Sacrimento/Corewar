30522ec32310b497dcecef80b7792d20a6270b1c
Sacrimento <sacrimentocorp@gmail.com>
2018-06-06 12:49:13 +0200

    process pc checked + winner func tested

--- src/main.c
+++ src/main.c
@@ -6,20 +6,20 @@
 /*   By: abouvero <abouvero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 17:40:13 by abouvero          #+#    #+#             */
-/*   Updated: 2018/06/06 11:12:42 by abouvero         ###   ########.fr       */
+/*   Updated: 2018/06/06 12:47:26 by abouvero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/corewar.h"
 
-// static void			winner(t_champ *ch)
-// {
+static void			winner(t_champ *ch)
+{
 
-// 	if (ch)
-// 		ft_printf("le joueur %d(%s) a gagne\n", ch->id, ch->name);
-// 	else
-// 		ft_printf("Les joueurs n'ont pas pu se departager, egalite !\n");
-// }
+	if (ch)
+		ft_printf("le joueur %d(%s) a gagne\n", ch->id, ch->name);
+	else
+		ft_printf("Les joueurs n'ont pas pu se departager, egalite !\n");
+}
 
 int					main(int argc, char **argv)
 {
@@ -32,7 +32,7 @@ int					main(int argc, char **argv)
 	if (!(vm = init_vm(argc, argv)))
 		return (1);
 	//run(vm);
-	//winner(vm->last);
+	winner(vm->last);
 	free_vm(vm);
 	return (0);
 }
