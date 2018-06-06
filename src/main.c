4ac97c5d391886df5cae5a25cfcf9578095b82a1
Sacrimento <sacrimentocorp@gmail.com>
2018-06-06 10:31:11 +0200

    GIMME MY MAIN BACK

--- src/main.c
+++ src/main.c
@@ -3,47 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mfonteni <mfonteni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abouvero <abouvero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 17:40:13 by abouvero          #+#    #+#             */
-/*   Updated: 2018/06/04 14:49:23 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/06/06 10:30:46 by abouvero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/corewar.h"
 
-// static int			check_inputs(void)
-// {
-// 	//Check des defsines d'op.h
-// 	return (0);
-// }
-
-
-void				mem_dump(unsigned char *map)
+static int			check_inputs(void)
 {
-	int		i;
+	if (0)
+		ft_dprintf(2, "Invalid values in \"op.h\"\n");
+	return (1);
+}
 
-	i = 0;
-	while (i < MEM_SIZE)
-	{
-		ft_printf("%2.2x", map[i++]);
-		if (!(i % 32) && i != 0)
-			ft_putchar('\n');
-		else
-			ft_putchar(' ');
-	}
+static void			winner(t_champ *ch)
+{
+
+	if (ch)
+		ft_printf("le joueur %d(%s) a gagne\n", ch->id, ch->name);
+	else
+		ft_printf("Les joueurs n'ont pas pu se departager, egalite !\n");
 }
 
 int					main(int argc, char **argv)
 {
 	t_vm	*vm;
 
-	//if ((opt = set_opt(argc, argv)) == -1 || check_inputs(argc, argv))
-	//	return (1);
+	if (argc < 2)
+		return (usage(1));
+	if (!check_inputs())
+		return (1);
 	if (!(vm = init_vm(argc, argv)))
 		return (1);
-	continue_process(vm, vm->processes);
-	//mem_dump(vm->map);
+	//run(vm);
+	//winner(vm->last);
 	free_vm(vm);
 	return (0);
 }
