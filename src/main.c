53c0a6ace41df5bac27948305a192b51763eef62
Razshal <fontenille.mathieu@gmail.com>
2018-06-11 12:57:40 +0200

    before merge

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abouvero <abouvero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mfonteni <mfonteni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 17:40:13 by abouvero          #+#    #+#             */
-/*   Updated: 2018/06/06 12:47:26 by abouvero         ###   ########.fr       */
+/*   Updated: 2018/06/08 14:44:00 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 static void			winner(t_champ *ch)
 {
-
 	if (ch)
 		ft_printf("le joueur %d(%s) a gagne\n", ch->id, ch->name);
 	else
