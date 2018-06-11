9bd5715aeb5a528c4c7a36281e49cff9f6e07194
Razshal <fontenille.mathieu@gmail.com>
2018-06-11 15:12:45 +0200

    cleaned convert.c

--- src/convert.c
+++ src/convert.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   convert.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abouvero <abouvero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mfonteni <mfonteni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 17:40:13 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/06/10 14:54:08 by abouvero         ###   ########.fr       */
+/*   Updated: 2018/06/11 15:12:33 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,11 @@
 
 int	bytetoint(unsigned char *map, int cursor, int amount_of_bytes)
 {
-/* OLD
-	if (!map)
-		return (0);
-	if (ammount_of_bytes == T_REG)
-		return (map[0]);
-	if (ammount_of_bytes == T_IND)
-		return (map[0] | (map[1] << 8));
-	if (ammount_of_bytes == T_DIR)
-		return ((map[0] << 24) | (map[1] << 16) | (map[2] << 8) | map[3]);
-*/
 	int decal;
 	int result;
 
+	if (!map)
+		return (0);
 	decal = (amount_of_bytes - 1) * 8;
 	result = map[cursor % MEM_SIZE] << decal;
 	while ((decal = decal - 8) >= 0)
@@ -50,9 +42,5 @@ int	inttobytes(int n, int cursor, unsigned char *map)
 	decal = -1;
 	while (++decal < 4)
 		map[(cursor + decal) % MEM_SIZE] = (n >> (24 - (decal * 8))) & 0xFF;
-/* 	map[cursor % MEM_SIZE] = (n >> 24) & 0xFF;
-	map[(cursor + 1) % MEM_SIZE] = (n >> 16) & 0xFF;
-	map[(cursor + 2) % MEM_SIZE] = (n >> 8) & 0xFF;
-	map[(cursor + 3) % MEM_SIZE] = n & 0xFF; */
 	return (1);
 }
