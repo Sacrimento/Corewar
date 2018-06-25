9b08722b8dd14356794525ee9bbc1f68dfa36456
Razshal <fontenille.mathieu@gmail.com>
2018-06-25 17:45:25 +0200

    light for for st

--- src/instructions.c
+++ src/instructions.c
@@ -6,25 +6,18 @@
 /*   By: mfonteni <mfonteni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/21 12:36:15 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/06/25 15:31:29 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/06/25 17:43:36 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
- * Glosssaire :
- * OCP = Octet de Codage des Parametres
- * ***********************************************************
- * TODO: 
- * Tests, tests, tests and another tests
-**/
-
 #include "../include/corewar.h"
-#define DECAL 3
 
 int	live(t_instr instr)
 {
 	t_champ *thischamp;
 
+	if (!instr.vm->visu)
+		ft_printf("attempting live for %d\n", byte_to_int(instr.vm->map, instr.process->pc + 1, 4));
 	instr.process->alive = 1;
 	instr.vm->lives++;
 	if (!instr.vm || !instr.vm->champ
@@ -45,11 +38,14 @@ int	ld(t_instr instr)
 	|| !valid_reg(--instr.params[1].value))
 		return (free_params(instr, 0));
 	if (instr.params[0].type == T_DIR)
-		instr.process->reg[instr.params[1].value] = instr.params[0].value/* % IDX_MOD*/;
+		instr.process->reg[instr.params[1].value] = instr.params[0].value;
 	else
 		instr.process->reg[instr.params[1].value]
 		= byte_to_int(instr.vm->map,
 		get_address(instr.process->pc + (instr.params[0].value % IDX_MOD)), 4);
+	if (!instr.vm->visu)
+		ft_printf("{YELLOW}LD loaded %d in register %d{EOC}\n",
+		instr.process->reg[instr.params[1].value], instr.params[1].value);
 	instr.process->carry = instr.process->reg[instr.params[1].value] == 0;
 	return (free_params(instr, 1));
 }
@@ -60,15 +56,24 @@ int	st(t_instr instr)
 	if (!compare_params(instr.params, instr.opcode)
 	|| !valid_reg(--instr.params[0].value))
 		return (free_params(instr, 0));
-	if (instr.params[1].type == T_REG && instr.params[1].value <= REG_NUMBER)
+	if (instr.params[1].type == T_REG && instr.params[1].value <= REG_NUMBER
+	&& valid_reg(instr.params[1].value - 1))
+	{
+		instr.params[1].value--;
 		instr.process->reg[instr.params[1].value]
 		= instr.process->reg[instr.params[0].value];
+	}
 	else if (instr.params[1].type == T_IND)
 		int_to_bytes(instr.process->reg[instr.params[0].value],
 		get_address(instr.process->pc + (instr.params[1].value % IDX_MOD)),
 		instr.vm->map, instr);
 	else
 		return (free_params(instr, 0));
+	if (!instr.vm->visu)
+		ft_printf("ST stored %d from register %d, at address %d\n",
+		instr.process->reg[instr.params[0].value],
+		instr.params[0].value,
+		get_address(instr.process->pc + (instr.params[1].value % IDX_MOD)));
 	return (free_params(instr, 1));
 }
 
@@ -141,7 +146,7 @@ int	xor(t_instr instr)
 int	zjmp(t_instr instr)
 {
 	if (instr.process->carry == 0)
-		return (decal_pc(instr, DECAL, 0));
+		return (decal_pc(instr, 3, 0));
 	instr.process->pc = get_address(instr.process->pc +
 		byte_to_int(instr.vm->map, instr.process->pc + 1, 2) % IDX_MOD);
 	instr.process->cycles_left = -1;
@@ -169,7 +174,8 @@ int	sti(t_instr instr)
 		return (free_params(instr, 0));
 	int_to_bytes(instr.process->reg[instr.params[0].value],
 	get_address(instr.process->pc +
-	((instr.params[1].value + instr.params[2].value) % IDX_MOD)), instr.vm->map, instr);
+	((instr.params[1].value + instr.params[2].value) % IDX_MOD)),
+	instr.vm->map, instr);
 	return (free_params(instr, 1));
 }
 
@@ -186,7 +192,7 @@ int	core_fork(t_instr instr)
 	instr.vm->processes->color = instr.process->color;
 	while (++i < REG_NUMBER)
 		instr.vm->processes->reg[i] = instr.process->reg[i];
-	return (decal_pc(instr, DECAL, 1));
+	return (decal_pc(instr, 3, 1));
 }
 
 int	lld(t_instr instr)
@@ -234,7 +240,7 @@ int	core_lfork(t_instr instr)
 	instr.vm->processes->color = instr.process->color;
 	while (++i <= REG_NUMBER)
 		instr.vm->processes->reg[i] = instr.process->reg[i];
-	return (decal_pc(instr, DECAL, 1));
+	return (decal_pc(instr, 3, 1));
 }
 
 int	aff(t_instr instr)
@@ -245,7 +251,7 @@ int	aff(t_instr instr)
 	|| !get_champ_by_num(instr.vm->champ, instr.process->reg[1]))
 		return (free_params(instr, 0));
 	if (!instr.vm->visu)
-		ft_printf("{CYAN}[%d] - %s:%c{EOC}\n", instr.process->reg[1],
+		ft_printf("{CYAN}AFF:[%d] - %s:%c{EOC}\n", instr.process->reg[1],
 		get_champ_by_num(instr.vm->champ, instr.process->reg[1])->name,
 		instr.process->reg[instr.params[0].value]);
 	return (free_params(instr, 1));
