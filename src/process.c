4d871a6db3e405d6fdaff3db54cbaed89bb89e50
Sacrimento <sacrimentocorp@gmail.com>
2018-06-26 15:11:04 +0200

    normed process.c

--- src/process.c
+++ src/process.c
@@ -3,14 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   process.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mfonteni <mfonteni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abouvero <abouvero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/30 15:22:38 by abouvero          #+#    #+#             */
-/*   Updated: 2018/06/20 14:49:13 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/06/26 15:10:08 by abouvero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/corewar.h"
+
+void		unset_pro_lives(t_process *pro)
+{
+	while (pro)
+	{
+		pro->alive = 0;
+		pro = pro->next;
+	}
+}
 
 void		check_process(t_vm *vm)
 {
@@ -23,10 +32,6 @@ void		check_process(t_vm *vm)
 		{
 			del = pro->next;
 			pro->next = pro->next->next;
-			//if (vm->map[del->pc] - 1 >= 0 && vm->map[del->pc] - 1 < 16)
-			// ft_printf("{MAGENTA}%dKILLED:%d|Pc:%d|Instr:%s|Instr cycles %d|Cycle to die %d|Cycles left %d{EOC}\n",
-			// del->alive, del->id, del->pc, g_op_tab[vm->map[del->pc] - 1].name,
-			// g_op_tab[vm->map[del->pc] - 1].nb_cycle, vm->ctd, del->cycles_left);
 			ft_memdel((void**)&del);
 			vm->processes_nbr -= 1;
 		}
@@ -36,31 +41,20 @@ void		check_process(t_vm *vm)
 	{
 		del = vm->processes;
 		vm->processes = vm->processes->next;
-		//if (vm->map[del->pc] - 1 >= 0 && vm->map[del->pc] - 1 < 16)
-		// ft_printf("{MAGENTA}%dKILLED:%d|Pc:%d|Instr:%s|Instr cycles %d|Cycle to die %d|Cycles left %d{EOC}\n",
-		// del->alive, del->id, del->pc, g_op_tab[vm->map[del->pc] - 1].name,
-		// g_op_tab[vm->map[del->pc] - 1].nb_cycle, vm->ctd, del->cycles_left);
 		ft_memdel((void**)&del);
 		vm->processes_nbr -= 1;
 	}
-	pro = vm->processes;
-	while (pro)
-	{
-		pro->alive = 0;
-		pro = pro->next;
-	}
+	unset_pro_lives(vm->processes);
 }
 
 int			add_process(t_vm *vm, int pc, int id)
 {
 	t_process	*pro;
 	t_process	*new;
-	static int i = 1;
 	
 	pro = vm->processes;
 	if (!(new = (t_process*)ft_memalloc(sizeof(t_process))))
 		return (error_mall(0));
-	new->id = i++;
 	new->alive = 1;
 	new->pc = pc;
 	new->carry = 0;
