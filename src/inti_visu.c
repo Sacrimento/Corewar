a0705ba52b9d9ebbaaacf8feef479c58760f5845
Razshal <fontenille.mathieu@gmail.com>
2018-06-27 19:15:57 +0200

    inti visu fix

--- src/inti_visu.c
+++ src/inti_visu.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   inti_visu.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abouvero <abouvero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mfonteni <mfonteni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/26 15:45:08 by rkrief            #+#    #+#             */
-/*   Updated: 2018/06/26 15:57:37 by abouvero         ###   ########.fr       */
+/*   Updated: 2018/06/27 19:15:40 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ t_visu *inti_visu(void)
 	if (!(visu = ft_memalloc(sizeof(t_visu))))
 		return (NULL);
 	visu->start = 0;
-	visu->win = init_visu(visu);
-	visu->score = init_score(visu);
+	visu->win = init_visu();
+	visu->score = init_score();
 	visu->slow = 30000;
 	return (visu);
 }
